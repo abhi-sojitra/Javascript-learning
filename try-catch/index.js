ef943a6d516116ddb2b1a7dea336ef01c060bd58
Abhi <abhisheksojitra19@gmail.com>
2023-05-31 19:39:53 +0530

    Try and catch & constructor

--- try-catch/index.js
+++ try-catch/index.js
@@ -5,6 +5,7 @@ function getCurrentDays() {
     if (isNaN(currentAge)) throw "only number input allowed";
     if (currentAge) {
       let calculate_days = currentAge * 365;
+      console.log(total_students,'total_students');
       document.getElementById("total_days").innerHTML = calculate_days;
       console.log("currentAge", currentAge);
     }
@@ -26,7 +27,7 @@ function blockScope() {
 function studentManage() {
   let total_class = 20;
   let avg_student = total_students / total_class;
-  console.log(avg_student, "avg_student");
+  // console.log(avg_student, "avg_student");
 }
 /** Funcation Call */
 studentManage();
@@ -44,4 +45,34 @@ function getUsers() {
     .then((response) => response.json())
     .then((json) => console.log(json));
 }
-getUsers();
+// getUsers();
+
+/** Class Object */
+class Car {
+  /** Constructor Property for Object  */
+  constructor(name, year ,dob) {
+    this.name = name;
+    this.year = year;
+    this.user_dob = dob;
+
+  }
+  /** Function with Argument */
+  age() {
+    return this.user_dob;
+  }
+}
+
+/** Get Date */
+const date = new Date();
+/** Date to Year */
+let year = date.getFullYear();
+
+let user_dob = "28-09-1999";
+
+/** Create New Object */
+const myCar = new Car("Ford", 2022 , user_dob );
+console.log(myCar.age());
+
+/** Arrow Function */
+let defaultArrowFunction = (a, b) => a * b;
+console.log("🚀 ~ file: index.js:77 ~ defaultArrowFunction:", defaultArrowFunction(a,b))
